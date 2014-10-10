275361bf7d5b20310485ba9c7b24556434128cff
Adriel Bergmann <adrielb@users.noreply.github.com>
2014-10-10 14:46:12 -0400

    coin flip example

--- 1-coinflip.py
+++ 1-coinflip.py
@@ -2,20 +2,18 @@ import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 7000
+dist = stats.beta
+x = np.linspace( 0, 1, 400 )
+
+N = 800
 data = stats.bernoulli.rvs( 0.5, size = N )
 data
 
-dist = stats.beta
-
-x = np.linspace( 0, 1, 400 )
-
-
 heads = data.sum()
-heads = 3
 
 y = dist.pdf( x, 1 + heads, 1 + N - heads )
 
+plt.clf()
 plt.plot( x, y )
 plt.show(block=False)
 plt.draw()
