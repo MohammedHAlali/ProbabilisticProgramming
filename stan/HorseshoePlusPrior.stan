1c52508da850cea7dfacdb20cc82a64b0d33733b
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-12-07 17:38:52 -0500

    Horseshoe+ Prior

---
+++ stan/HorseshoePlusPrior.stan
@@ -0,0 +1,36 @@
+// http://arxiv.org/pdf/1502.00560v2.pdf
+functions { # {{{
+
+} # }}}
+
+data { #{{{
+  int<lower=1> N;
+} #}}}
+
+transformed data { #{{{
+
+} #}}}
+
+parameters { #{{{
+  vector[N] w_raw;
+  vector<lower=0>[N] lambda;
+  vector<lower=0>[N] eta;
+  real<lower=0> tau;
+} #}}}
+
+transformed parameters { #{{{
+  vector[N] w;
+  w <- tau * (eta .* lambda .* w_raw);
+} #}}}
+
+model { #{{{
+  w_raw ~ normal(0,1); // implies w ~ normal(0,tau)
+  lambda ~ cauchy( 0, 1 ); // local shrinkage
+  eta ~ cauchy(0,1); // global shrinkage
+  tau ~ cauchy(0,1); // global shrinkage
+  // tau <- p_n / n; // empirical Bayes estimator
+} #}}}
+
+generated quantities { #{{{
+
+} #}}}
--- stan/HorseshoePrior.R
+++ stan/HorseshoePrior.R
@@ -10,6 +10,15 @@ options(max.print=450)
 rstan_options(auto_write=TRUE)
 # }}}
 
+# refs {{{
+
+# https://ariddell.org/horseshoe-prior-with-stan.html
+# http://andrewgelman.com/2015/02/17/bayesian-survival-analysis-horseshoe-priors/#comment-211738
+# https://s3.amazonaws.com/docs.jrnold.me/Arnold_PolmethXXX_Poster.pdf
+# http://citeseerx.ist.psu.edu/viewdoc/download;jsessionid=65C457F43CE140B0FD0FEBCA8E4BC96C?doi=10.1.1.217.2084&rep=rep1&type=pdf
+
+# }}}
+
 dso <- stan_model( stanc_ret=stanc(file="./HorseshoePrior.stan"))
 
 datalist <- list( N=2
@@ -44,7 +53,7 @@ df.stats %>% select( param, ti) %>% distinct(param)
 
 # }}}
 
-
+# plots {{{
 df <- NULL
 df <- as.data.frame(samples) %>% tbl_df() %>% 
   mutate(sample=factor(1:(chains*iter))) %>% 
@@ -68,4 +77,63 @@ qplot(c(0, 1)
 df %>% filter( -100 < w_1, w_1 < 100 ) %>% 
   qplot( x=w_1, data=., geom="histogram", binwidth=1)
 
+# }}}
 
+# Horseshoe+ prior {{{
+
+dso.HorseshoePlusPrior <- NULL
+dso.HorseshoePlusPrior <- stan_model( stanc_ret=stanc(file="./HorseshoePlusPrior.stan"))
+
+datalist <- list( N=2 )
+print(datalist)
+
+# sampling {{{
+chains <- 4
+iter <- 2e3
+warmup <- 1e3
+samples <- NULL
+samples <- sampling(dso.HorseshoePlusPrior
+  , data=datalist
+  , chains=chains
+  , iter=warmup+iter
+  , warmup=warmup
+)
+
+print(samples)
+
+traceplot(samples)
+
+pairs(samples, pars=c("lp__","w[1]","w[2]"
+                      ,"lambda[1]","lambda[2]"))
+
+df.stats <- monitor(samples,print=F) %>% as.data.frame() 
+df.stats$params <- row.names(df.stats)
+df.stats <- df.stats %>% separate( params, into=c("param", "ti")
+    , sep = "[\\[\\]]", remove=TRUE, convert=TRUE, extra="drop") %>% 
+    print()
+df.stats %>% select( param, ti) %>% distinct(param)
+
+# }}}
+
+# plots {{{
+df <- NULL
+df <- as.data.frame(samples) %>% tbl_df() %>% 
+  mutate(sample=factor(1:(chains*iter))) %>% 
+    select( everything(), w_1=`w[1]`, w_2=`w[2]` ) %>% 
+    print()
+
+ggplot( data=df, aes(x=w_1, y=w_2)) +
+  geom_point(aes(color=lp__),size=4,alpha=0.1)
+
+ggplot( data=df, aes(x=w_1, y=w_2)) +
+  geom_density2d()
+
+df %>% filter( -100 < w_1, w_1 < 100 ) %>% 
+  qplot( x=w_1, data=., geom="histogram", binwidth=1)
+
+# }}}
+
+
+
+
+# }}}
