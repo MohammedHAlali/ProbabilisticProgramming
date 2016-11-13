92184faa56622ca476a2070a50b62949d858572c
Adriel Bergmann <adrielb@users.noreply.github.com>
2016-11-13 18:41:16 -0500

    spacy: word vectors

---
+++ spacy/word_vectors.py
@@ -0,0 +1,13 @@
+import numpy
+
+apples, and_, oranges = nlp(u'apples and oranges')
+
+print( apples.vector )
+
+print( apples.vector.shape )
+
+apples.similarity( oranges )
+
+vocab.load_vectors()
+
+lexeme
