0c050ff224a722b4d8f664f672e5072aa797f595
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-01-06 16:16:44 -0500

    theano: kkastner - use tag test values

--- theano/repl.sh
+++ theano/repl.sh
@@ -25,3 +25,11 @@ python ~/.local/lib/python3.5/site-packages/theano/misc/elemwise_openmp_speedup.
 # Timed with vector of 200000 elements
 # Fast op time without openmp 0.000182s with openmp 0.000115s speedup 1.58
 # Slow op time without openmp 0.006456s with openmp 0.001645s speedup 3.93
+
+# https://www.reddit.com/r/MachineLearning/comments/4ekywt/tensorflow_vs_theano_which_to_learn/d21xhin/?st=ixm8s60u&sh=7d978275
+export THEANO_FLAGS="device=cpu,floatX=float32,optimizer=None,compute_test_value=raise"
+time python3 ./updates_test.py
+
+export THEANO_FLAGS=""
+time python3 ./updates_test.py
+
