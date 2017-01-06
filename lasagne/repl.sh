c2f5b68dbafe2c44ba5b1bdb492221c486721c71
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-01-06 17:33:01 -0500

    lasagne: recurrent.py

--- lasagne/repl.sh
+++ lasagne/repl.sh
@@ -4,3 +4,7 @@ python3 ~/.local/lib/python3.6/site-packages/lasagne/
 
 wget https://raw.githubusercontent.com/Lasagne/Lasagne/master/examples/mnist.py 
 
+wget https://raw.githubusercontent.com/Lasagne/Lasagne/master/examples/recurrent.py
+
+python3 ./recurrent.py
+
