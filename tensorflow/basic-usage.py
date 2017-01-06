bee164212fffcdd4e8a52468d0fb98700a014945
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-01-06 18:37:36 -0500

    tensorflow: basic usage

---
+++ tensorflow/basic-usage.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+matrix1 = tf.constant( [[3.,3.]] )
+matrix2 = tf.constant( [[2.], [2.]] )
+
+product = tf.matmul( matrix1, matrix2 )
+
+sess = tf.Session()
+
+result = sess.run( product )
+
+sess.close()
+
+with tf.Session() as sess:
+    with tf.device('/cpu:0'):
+        result = sess.run( [product] )
+        print( result )
+
+
+
