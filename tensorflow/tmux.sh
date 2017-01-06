97d4e063d7076d3d4e54b9c0dbd0a84a90dbf89f
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-01-06 17:36:02 -0500

    tensorflow: tmux.sh updated

--- tensorflow/tmux.sh
+++ tensorflow/tmux.sh
@@ -3,17 +3,11 @@
 NAME=tensorflow
 
 if tmux has-session -t $NAME; then
-  tmux kill-window -a -t $NAME:R
+  tmux kill-session -t $NAME
 else
   tmux new-session -A -s $NAME \; \
-    send-keys 'R -q' C-m \; \
-    rename-window 'R' \; \
-    new-window -n 'julia' \; \
-    send-keys 'while true;do julia;sleep 1;done' C-m \; \
     new-window -n 'ipython' \; \
-    send-keys 'ipython --no-banner' C-m \; \
-    new-window -n 'bash' \; \
-    send-keys 'bash' C-m \; \
-    select-window -t 3
+    send-keys 'ipython3 --no-banner' C-m \; \
+    select-window -t 2
 fi
 
