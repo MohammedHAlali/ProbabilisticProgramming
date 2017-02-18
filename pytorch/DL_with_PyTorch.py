190facde54ce8a4be1177916bccc0007f1d8ce1b
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-02-17 22:21:21 -0500

    pytorch: Deep Learning: A 60-minute blitz

--- pytorch/DL_with_PyTorch.py
+++ pytorch/DL_with_PyTorch.py
@@ -1,3 +1,6 @@
+# Deep Learning with PyTorch: A 60-minute Blitz
+# https://github.com/pytorch/tutorials/blob/master/Deep%20Learning%20with%20PyTorch.ipynb
+
 import numpy as np
 import torch
 
@@ -34,5 +37,276 @@ print(b)
 
 torch.cuda.is_available()
 
+
+# Autograd
+
+from torch.autograd import Variable
+
+x = Variable(torch.ones(2,2), requires_grad = True)
+
+y = x + 2
+
+y.creator
+
+z = y * y * 3
+
+out = z.mean()
+
+out.backward()
+
+x.grad
+
+x = torch.randn(3)
+
+x = Variable(x, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+
+gradients = torch.FloatTensor( [0.1, 1.0, 0.0001] )
+
+y.backward( gradients )
+
+x.grad
+
+#
+# Neural Networks
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5) # 1 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120) # an affine operation: y = Wx + b
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2)) # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2) # If the size is a square you can only specify a single number
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net = Net()
+net
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())
+
+input = Variable(torch.randn(1,1,32,32))
+out = net(input)
+
+net.zero_grad()
+
+out.backward(torch.randn(1,10))
+
+# inputs are only minibatches
+# convert single sample to add fake batch dim:
+input.unsqueeze(0)
+
+# loss functions
+
+output = net(input)
+target = Variable(torch.range(1, 10))  # a dummy target, for example
+criterion = nn.MSELoss()
+loss = criterion(output, target)
+loss
+
+
+loss.creator
+
+output.creator
+
+# input -> conv2d -> relu -> maxpool2d -> conv2d -> relu -> maxpool2d  
+#       -> view -> linear -> relu -> linear -> relu -> linear 
+#       -> MSELoss
+#       -> loss
+
+
+# For illustration, let us follow a few steps backward
+print(loss.creator) # MSELoss
+print(loss.creator.previous_functions[0][0]) # Linear
+print(loss.creator.previous_functions[0][0].previous_functions[0][0]) # ReLU
+
+
+
+# now we shall call loss.backward(), and have a look at conv1's bias gradients
+# before and after the backward.
+net.zero_grad() # zeroes the gradient buffers of all parameters
+print('conv1.bias.grad before backward')
+print(net.conv1.bias.grad)
+loss.backward()
+print('conv1.bias.grad after backward')
+print(net.conv1.bias.grad)
+
+
+
+import torch.optim as optim
+# create your optimizer
+optimizer = optim.SGD(net.parameters(), lr = 0.01)
+
+# in your training loop:
+optimizer.zero_grad() # zero the gradient buffers
+output = net(input)
+loss = criterion(output, target)
+loss.backward()
+optimizer.step() # Does the update
+
+
+import torchvision
+import torchvision.transforms as transforms
+
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1]
+transform=transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                             ])
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, 
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, 
+                                          shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# functions to show an image
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.ion()
+
+def imshow(img):
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+
+# show some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s'%classes[labels[j]] for j in range(4)))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool  = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+# use a Classification Cross-Entropy loss
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2): # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+        
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()        
+        optimizer.step()
+        
+        # print statistics
+        running_loss += loss.data[0]
+        if i % 2000 == 1999: # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
+            running_loss = 0.0
+print('Finished Training')
+
+
+
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s'%classes[labels[j]] for j in range(4)))
+
+
+outputs = net(Variable(images))
+
+# the outputs are energies for the 10 classes. 
+# Higher the energy for a class, the more the network 
+# thinks that the image is of the particular class
+
+# So, let's get the index of the highest energy
+_, predicted = torch.max(outputs.data, 1)
+
+print('Predicted: ', ' '.join('%5s'% classes[predicted[j][0]] for j in range(4)))
+
+
+correct = 0
+total = 0
+for data in testloader:
+    images, labels = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+
+
+
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+for data in testloader:
+    images, labels = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    c = (predicted == labels).squeeze()
+    for i in range(4):
+        label = labels[i]
+        class_correct[label] += c[i]
+        class_total[label] += 1
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
 
 
