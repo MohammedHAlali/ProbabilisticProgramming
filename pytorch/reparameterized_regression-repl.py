ca9d537c47ed593991352d4b00079925c6f431df
Adriel Bergmann <adrielb@users.noreply.github.com>
2017-06-11 18:13:48 -0400

    pytorch: reparameterized_regression

--- pytorch/reparameterized_regression-repl.py
+++ pytorch/reparameterized_regression-repl.py
@@ -14,21 +14,19 @@ plt.ion()
 import reparameterized_regression
 
 
-N = 100
-input_data = 10*np.random.random(N)
+N = 10
+input_data = np.random.uniform(4,6,N)
 output_data = 3*input_data-2 + np.random.randn(N)
 plt.clf()
+plt.xlim(0,10)
+plt.ylim(-3,30)
 plt.scatter(input_data, output_data)
 
 from sklearn import linear_model
-
 linreg = linear_model.LinearRegression()
-
 linreg.fit(input_data.reshape(-1,1), output_data)
-
 linreg.coef_
 # Out[10]: array([ 2.9758882])
-
 linreg.intercept_
 # Out[11]: -2.0223463314976993
 
@@ -46,6 +44,8 @@ Y_data = make_var(output_data)
 def show_plot(epoch):
     Y = model.l1(X_data)
     plt.clf()
+    plt.xlim(0,10)
+    plt.ylim(-3,30)
     plt.title(epoch)
     plt.scatter(input_data, output_data)
     plt.plot(X_data.squeeze().data.numpy(), Y.squeeze().data.numpy(),
@@ -67,5 +67,63 @@ for epoch in range(200):
     optimizer.step()
     show_plot(epoch)
     print( epoch, model.l1.weight.data[0,0], model.l1.bias.data[0])
+
+
+
+b = reparameterized_regression.RepNormal()
+b()
+
+bayesmodel = reparameterized_regression.BayesianModel()
+bayesmodel(X_data)
+
+def show_plot(epoch):
+    plt.clf()
+    plt.title(epoch)
+    plt.ylim(-20,40)
+    for _ in range(100):
+        X_test = 10*np.random.random(N)
+        X_test = Variable(torch.FloatTensor(X_test))
+        X_test = X_test.unsqueeze(1)
+        Y = bayesmodel(X_test)
+        plt.scatter(X_test.squeeze().data.numpy(), Y.squeeze().data.numpy(),
+            color='green', alpha=0.1)
+    plt.scatter(input_data, output_data)
+    plt.pause(0.01)
+show_plot(1)
+
+learning_rate=0.02
+optimizer = torch.optim.Adam(bayesmodel.parameters(), lr=learning_rate)
+loss_fn = torch.nn.MSELoss()
+L = 10
+for epoch in range(200):
+    optimizer.zero_grad()
+    for _ in range(L):
+        output = bayesmodel(X_data)
+        loss = loss_fn(output, Y_data) / L
+    loss.backward()
+    optimizer.step()
+    show_plot(epoch)
+    print( epoch
+            , loss.data[0]
+            , bayesmodel.w_param.mu.data[0]
+            , bayesmodel.b_param.mu.data[0])
+
+bayesmodel.b_param.log_variance.data[0] = 1
+
+#
+#
+#
+mu = Variable(torch.FloatTensor([1.0]), requires_grad=True)
+loss = torch.pow(mu - 3.0, 2)
+loss.backward()
+mu.grad
+
+x = Variable(torch.FloatTensor([[1.0]]))
+l1 = torch.nn.Linear(1,1)
+mu = l1(x)
+loss = torch.pow(mu - 3.0, 2)
+loss.backward()
+print("mu.grad: ", mu.grad)
+print("w grad: ", l1.weight.grad)
 
 
--- pytorch/reparameterized_regression.py
+++ pytorch/reparameterized_regression.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
+from torch.nn import Parameter
+from torch import FloatTensor
 
 
 class Model(torch.nn.Module):
@@ -20,3 +22,34 @@ class Model(torch.nn.Module):
         return Y
 
 
+class BayesianModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.b_param = RepNormal()
+        self.w_param = RepNormal()
+
+    def forward(self, X):
+        return self.simulate(X)
+
+    def simulate(self, X):
+        b = self.b_param().expand(X.size())
+        w = self.w_param().unsqueeze(0)
+        Y = b + X.mm(w) + Variable(torch.randn(b.size()))
+        return Y
+
+
+class RepNormal(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mu = Parameter(FloatTensor([0.0]))
+        self.log_variance = Parameter(FloatTensor([0.0]))
+
+    def __call__(self):
+        z = Variable(torch.randn(1))
+        return self.mu + self.log_variance.exp() * z
+
+    def _repr_pretty_(self, p, cycle):
+        p.text("mu = {}".format(self.mu))
+        p.text("std = {}".format(self.log_variance.exp()))
+
+class Poster
